53583657d4b258fef54ac2838dbd05f0355df0dc
Hoa Dam <hoangocdam@gmail.com>
2024-05-12 22:45:12 -0700

    create teas db

---
+++ db/migrate/20240509084723_create_teas.rb
@@ -0,0 +1,11 @@
+class CreateTeas < ActiveRecord::Migration[7.1]
+  def change
+    create_table :teas do |t|
+      t.string :description
+      t.integer :temperature
+      t.integer :brew_time
+
+      t.timestamps
+    end
+  end
+end
